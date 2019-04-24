146a0a731bf486be2d49a6306914ce2adcc7fd0a
Ruben C. Arslan <rubenarslan@gmail.com>
2019-04-24 11:48:57 +0200

    readd packrat bibl

--- R/sanity.R
+++ R/sanity.R
@@ -68,3 +68,86 @@ view_in_excel <- function(x) {
     system(paste('open -a "Microsoft Excel"', excel))
   }
 }
+
+
+#' build a bibliography bibtex file from your packrat lockfile
+#'
+#' Packrat helps you maintain consistent package versions for a project. To be able to give due credit in a way that academics understand, it's helpful to be able to generate citations.
+#'
+#'
+#' @param overwrite_bib whether to overwrite an existing bibtex file of the same name
+#' @param silent defaults to false. whether to cat out a nocite string to use in your header
+#' @param cite_only_directly_called whether to call only the packages you called yourself (default) or also their dependencies
+#' @param lockfile_path path to the packrat lock file to use
+#' @param bibliography_path path to the bibtex file to generate
+#' @param cite_packrat whether to cite packrat even if it's not loaded explicitly, defaults to the reverse of cite_only_directly_called
+#' @export
+#'
+packrat_bibliography = function(overwrite_bib = FALSE,
+                                silent = FALSE,
+                                cite_only_directly_called = TRUE,
+                                lockfile_path = "packrat/packrat.lock",
+                                bibliography_path = "packrat_bibliography.bibtex",
+                                cite_packrat = !cite_only_directly_called) {
+
+  if (file.exists(bibliography_path) && !overwrite_bib) {
+    stop("Bibliography file existed and won't be overwritten, specify overwrite_bib = TRUE.")
+  }
+  # use internal function to read lockfile (uses readDcf)
+  if (cite_only_directly_called) {
+    package_names = packrat:::dirDependencies("./")
+  } else {
+    stopifnot(file.exists(lockfile_path))
+    packages = packrat:::readLockFilePackages(lockfile_path)
+    package_names = names(packages) # get pkg names
+  }
+
+  citation_objects = list()
+  citation_objects$R = utils::citation()
+  citation_objects$R$note = paste("version", as.character(getRversion()))
+  citation_objects$R$key = "R"
+
+  if (cite_packrat) {
+    package_names = union("packrat", package_names)
+  }
+
+  for (i in seq_along(package_names)) {
+    pkg_name = package_names[i]
+    citation_obj = utils::citation(pkg_name)
+    citation_obj$key = pkg_name	# by default the bibtex entries lack keys, we use the pkg name ,
+    pkg_version = as.character(utils::packageVersion(pkg_name))
+    if (is.null(citation_obj$note)) {
+      citation_obj$note = ''
+    }
+    citation_obj$note = paste(citation_obj$note, "version", pkg_version)
+    citation_objects[[ pkg_name ]] = citation_obj
+  }
+
+  bibliography = list()
+  for (i in seq_along(citation_objects)) {
+    name = names(citation_objects)[i]
+    citation_obj = citation_objects[[i]]
+    # don't lowercase R
+    citation_obj[1]$title = stringr::str_replace_all(citation_obj[1]$title, "\\bR\\b", "{R}")
+    # don't uppercase the package title
+    citation_obj[1]$title = stringr::str_replace_all(citation_obj[1]$title, "^([a-zA-Z0-9.]+)+:", "{\\1}:")
+    class(citation_obj) = c("citation", "bibentry")
+    bibliography[[name]] = paste0(
+      as.character(utils::toBibtex(citation_obj)),
+      collapse = "\n")
+  }
+
+  bibliography = paste0(bibliography, collapse = "\n\n") # concatenate citations
+
+  # write bibliography to file
+  writeLines(iconv(bibliography, to = "UTF-8"), bibliography_path, useBytes = TRUE)
+
+  # generate YAML reference with nocite
+  if (!silent) {
+    cat(paste0("
+							 bibliography: ", bibliography_path, "
+							 nocite: |
+							 ", paste0("@", c("R", package_names), collapse = " ")))
+  }
+  invisible(bibliography)
+}
