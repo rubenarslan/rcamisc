2bb97b274267ee17375572c6266e8e17d617a8e3
Ruben C. Arslan <rubenarslan@gmail.com>
2023-02-27 12:25:46 +0100

    make render_job an addin

--- R/render_as_job.R
+++ R/render_as_job.R
@@ -2,10 +2,11 @@
 #'
 #' if you want to
 #'
-#' @param input .rmd document to be knitted
-#' @param params params to pass to the .rmd
+#' @param input .Rmd document to be knitted
+#' @param params params to pass to the .Rmd
 #' @param output_file name of the output_file (and the job)
 #'
+#' @importFrom utils capture.output
 #'
 #' @export
 #' @examples
@@ -13,20 +14,26 @@
 #'    render_job("document.Rmd", list(dataset = "df1"), "summary_df1.html")
 #' }
 #'
-render_job <- function(input, params = list(), output_file) {
-  tmp <- tempfile()
-  .rcamisc_global_input_file_for_rstudio_job <<- input
-  .rcamisc_global_params_for_rstudio_job <<- params
-  .rcamisc_global_output_file_for_rstudio_job <<- output_file
-  rmarkdown_render <- rmarkdown::render
-  cat('rmarkdown::render(input = .rcamisc_global_input_file_for_rstudio_job,
-                      params = .rcamisc_global_params_for_rstudio_job,
-                      output_file = .rcamisc_global_output_file_for_rstudio_job
-      )',
+render_job <- function(input= rstudioapi::getSourceEditorContext()$path,
+                       params = NULL,
+                       output_file = NULL) {
+  tmp <- tempfile(fileext = ".R")
+  name <- output_file
+  if(is.null(name)) {
+    name <- input
+  }
+
+  params_string <- capture.output(dput(params))
+  output_file_string <- capture.output(dput(output_file))
+  so_that_its_used <- rmarkdown::render
+  cat(paste0('rmarkdown::render(input = "',input,'",
+                      params = ',params_string,',
+                      output_file = ',output_file_string,'
+      )'),
       file = tmp,
       sep = "\n")
   rstudioapi::jobRunScript(tmp,
                            workingDir = getwd(),
-                           output_file, importEnv = TRUE)
+                           name, importEnv = FALSE)
 }
 
